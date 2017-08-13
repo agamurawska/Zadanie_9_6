41a73231e1e52f9391b75c0b197389e6034d4d1a
Agnieszka <aga.murawska@gmail.com>
2017-08-13 23:43:49 +0200

    new li item by button in js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+﻿var list = document.getElementById('list'),
+add = document.getElementById('addElem');
+
+add.addEventListener('click', function(){
+var element = document.createElement('li');
+
+var liNo = document.getElementsByTagName('li');
+var number =liNo.length;
+
+element.innerHTML = 'item '+ number;
+list.appendChild(element);
+}
+);
